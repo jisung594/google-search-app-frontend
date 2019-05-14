18b27cde6084e68fd38d87b6d95ff671ccc47edb
jisung594 <jisung594@gmail.com>
2019-05-14 14:46:05 -0400

    initial commit

---
+++ src/Components/Homepage.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+class Homepage extends Component {
+  state = {
+    searchTerm: ""
+  }
+
+  changeHandler = (event) => {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>THIS IS THE HOMEPAGE</h2>
+        <input type="text" name="" value={this.state.searchTerm} onChange={this.changeHandler}/>
+      </div>
+    )
+  }
+}
+
+export default Homepage
