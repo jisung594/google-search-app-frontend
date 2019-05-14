a35140c4bc49e206fe4868a0f81c0d7795d246dc
jisung594 <jisung594@gmail.com>
2019-05-14 15:26:54 -0400

    connected with backend

--- src/App.js
+++ src/App.js
@@ -3,10 +3,32 @@ import './App.css';
 import Homepage from './Components/Homepage'
 
 function App() {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      apiResponse: ""
+    }
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({
+        apiResponse: res
+      }))
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h2>hi, this is from the APP component</h2>
+        <h2>hi, this is from the APP component (pure React)</h2>
+        <p>line below is the response from API</p>
+        <p>{this.state.apiResponse}</p>
         <Homepage/>
       </header>
     </div>
